4c1d2910d2b9d8f78f71039a741911b551ebdff9
mythbustersjk <mythbruther@foxmail.com>
2022-02-14 14:54:12 +0800

    change_configuration_file

--- DefaultRange.py
+++ DefaultRange.py
@@ -2,13 +2,13 @@
 企业规模划型标准定义函数
 """
 
-import json
+import yaml
 
 
 def industry_range(industry_name):
     try:
-        with open('range.json') as range_file:
-            ids_dict = json.load(range_file)
+        with open('DefaultSetting.yaml', 'r', encoding='utf-8') as range_file:
+            ids_dict = yaml.load(range_file, Loader=yaml.FullLoader)
         return ids_dict[industry_name]
     except FileNotFoundError:
         ids_dict = {'yuye': {'income_max': 20000, 'income_2nd': 500, 'income_min': 50},
@@ -42,8 +42,6 @@ def industry_range(industry_name):
                                'total_assets_max': 120000, 'total_assets_2nd': 8000, "total_assets_min": 100},
                     'other': {'num_people_max': 300, 'num_people_2nd': 100, 'num_people_min': 10}
                     }
-        with open('range.json', "w") as create_range_file:
-            json.dump(ids_dict, create_range_file)
+        with open('DefaultSetting.yaml', "w", encoding='utf-8') as create_range_file:
+            yaml.dump(data=ids_dict, stream=create_range_file, allow_unicode=True)
         return ids_dict[industry_name]
-
-
